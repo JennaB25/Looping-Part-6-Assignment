5471ba3683f352335065775353a489731e4877c7
362700239 <jennarbloor@gmail.com>
2023-03-06 11:01:57 -0500

    finish percent passing

--- Looping Part 6 Assignment/Program.cs
+++ Looping Part 6 Assignment/Program.cs
@@ -1,4 +1,9 @@
-﻿using System.Transactions;
+﻿using Microsoft.Win32.SafeHandles;
+using System.Diagnostics.CodeAnalysis;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading.Tasks.Sources;
+using System.Transactions;
+using static System.Formats.Asn1.AsnWriter;
 
 namespace Looping_Part_6_Assignment
 {
@@ -33,6 +38,7 @@ namespace Looping_Part_6_Assignment
                     Console.WriteLine("You chose Precent Passing");
                     Console.WriteLine("Hit ENTER to continue.");
                     Console.ReadLine();
+                    precentPassing();
                 }                
                 else
                 {
@@ -48,13 +54,57 @@ namespace Looping_Part_6_Assignment
             Console.WriteLine("Min Value:");
             double minValue = Convert.ToDouble(Console.ReadLine());
             Console.WriteLine("Max Value:");
+       //check that the max is bigger than min
+       //add do while loop
             double maxValue = Convert.ToDouble(Console.ReadLine());
             Console.WriteLine("Enter a number within the previous values:");
             double value = Convert.ToDouble(Console.ReadLine());
             while (value < minValue || value > maxValue)
             {
                 Console.WriteLine("Try Again");
-            }            
+                //double value = Convert.ToDouble(Console.ReadLine());
+            }
+            if (value > minValue || value < maxValue)
+            {
+                Console.WriteLine("Correct");
+            }
+            else
+            {
+                Console.WriteLine("Error: Invalid Input");
+            }
+        }
+
+        public static void precentPassing()
+        {
+            Console.Clear();
+            bool done = false;      
+                Console.WriteLine("                       ----Precent-Passing----                    ");
+                Console.WriteLine("Enter a set of scores and I will tell you the precentage above 70%");
+            while (!done)
+            {
+                Console.WriteLine("Enter a score:");
+                string score = (Console.ReadLine());
+                score = score.Replace("%", "");
+                int _score = Convert.ToInt32(score);
+                Console.WriteLine("Done entering scores: One");
+                Console.WriteLine("Not done entering scores: Two");
+                string input = Console.ReadLine().ToUpper();
+                
+                if (input == "ONE")
+                {
+                    done = true;
+                }                   
+                else if (input == "TWO")
+                {
+                    while (false);
+                }              
+                else
+                {
+                    Console.WriteLine("Error: Invalid Input");
+                }               
+            }
+            int sumOfScores;
+            Console.WriteLine("test");
         }
     }
 }
